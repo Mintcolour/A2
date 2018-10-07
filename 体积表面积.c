e6ed6973b55d41047e999596c9cdba797144a31e
mintcolor <43575730+Mintcolour@users.noreply.github.com>
2018-10-07 20:53:32 +0800

    Create 体积表面积.c

---
+++ 体积表面积.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+main ()
+{
+double l, w, h;
+double s, v;
+printf("please input l:");
+scanf("%lf", &l);
+printf("please input w:");
+scanf("%lf", &w);
+printf("please input h:");
+scanf("%lf", &h);
+s = ((l * w) + (l * h) + (w * h)) * 2;
+v = l * w * h;
+printf("s = %lf\nv = %lf\n", s, v);
+getch();
+return 0;
+}
